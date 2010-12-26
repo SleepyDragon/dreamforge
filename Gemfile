8caf61a09c712b0856b5a70170cc05bef43a67a1
moonglum <moonglum@moonbeamlabs.com>
2010-12-26 12:23:18 +0100

    Initial Rails App

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'haml' # HAML & SASS
+gem 'jquery-rails', '>= 0.2.6'
+gem 'formtastic', '~> 1.1.0'
+gem 'cancan'
+gem "will_paginate", "~> 3.0.pre2"
+
+group :development, :test do
+  gem 'yard'
+  gem 'rspec'
+  gem "rspec-rails", "~> 2.0.1"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dreamforge::Application.config.secret_token = '88b87c4ebae363070fc3fd079750a3f82f743ebc2aa11d8c82a9ebbedde828bf337a8eb59d1f82c305b16773c5f247d15331c75c86e778f5f321c031dda2869f'
