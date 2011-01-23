d7c88797d6169436e44804e6a3aa0ab6503f48e4
moonglum <moonglum@moonbeamlabs.com>
2011-01-23 11:38:47 +0100

    Added Documentation to Category Model [#1]

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
+# @attr [String] name is shown in the community, required
+# @attr [String] subtitle is shown in the community, optional
+
 class Category < ActiveRecord::Base
   
   validates_presence_of :name
