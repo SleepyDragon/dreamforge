5189d3d9c0d39c1ad1ef25c17bb0b1c769c29fbb
moonglum <moonglum@moonbeamlabs.com>
2011-03-31 18:04:11 +0200

    Spec'ed and Implemented Index View

--- spec/views/forums/index.html.haml_spec.rb
+++ spec/views/forums/index.html.haml_spec.rb
@@ -2,17 +2,26 @@ require 'spec_helper'
 
 describe "forums/index.html.haml" do
   
-  let(:first_category) { stub_model(Category, :name => "First Category", :subtitle => "My Subtitle") }
-  
   before :each do
-    assign( :categories, [first_category] )
+    @first_forum = stub_model(Forum, :name => "First Forum", :subtitle => "My Subtitle")
+    @first_category = stub_model(Category, :name => "First Category", :subtitle => "My Subtitle", :forums => [@first_forum])
+    
+    assign( :categories, [@first_category] )
     render
   end
   
-  it "should display a table header for categories" do
+  it "should display a table header for each category" do
     
     rendered.should have_selector('thead') do |thead|
-      thead.should contain(first_category.name)
+      thead.should contain(@first_category.name)
+    end
+    
+  end
+  
+  it "should list all corresponding models" do
+    
+    rendered.should have_selector('tbody tr') do |tbody|
+      tbody.should contain(@first_forum.name)
     end
     
   end
