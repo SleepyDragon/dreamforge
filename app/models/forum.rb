472e3c4cb31bd1952f1031b1265f07a7c330fe89
moonglum <moonglum@moonbeamlabs.com>
2011-03-31 22:29:40 +0200

    * Further Features added to Index and Show * Corresponding View- and ControllerSpecs are written * Started writing the translation file

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,7 +1,7 @@
 # @attr [String] name is shown in the community, required
 # @attr [String] subtitle is shown in the community, optional
-# @attr [Category] category is the Category it depends on
-# @attr [Array] topics is a list of dependend topics, that will be destroyed, when the forum is destroyed
+# @attr [Category] category is the {Category} it depends on
+# @attr [Array] topics is a list containg each and every {Topic} depending on it. Will be destroyed, when the forum is destroyed.
 
 class Forum < ActiveRecord::Base
   validates_presence_of :name
--- spec/views/forums/show.html.haml_spec.rb
+++ spec/views/forums/show.html.haml_spec.rb
@@ -4,7 +4,13 @@ describe "forums/show.html.haml" do
   
   before :each do
     @corresponding_category = stub_model(Category, :name => "First Category", :subtitle => "My Subtitle")
-    @forum_that_should_be_shown = stub_model(Forum, :name => "First Forum", :subtitle => "My Subtitle", :category => @corresponding_category)
+    
+    @topics = []
+    13.times do |number|
+      @topics << stub_model(Topic, :title => "Topic #{number}")
+    end
+    
+    @forum_that_should_be_shown = stub_model(Forum, :name => "First Forum", :subtitle => "My Subtitle", :category => @corresponding_category, :topics => @topics)
     
     assign( :forum, @forum_that_should_be_shown )
     render
@@ -21,8 +27,22 @@ describe "forums/show.html.haml" do
     rendered.should have_selector('.breadcrumbs') do |breadcrumb|
       breadcrumb.should have_selector('li', :content => @forum_that_should_be_shown.name)
       breadcrumb.should have_selector('li', :content => @corresponding_category.name)
+      breadcrumb.should have_selector('li a', :content => t(:community), :href => "/forums")
     end
     
   end
+  
+  it "should display a table with the first ten topics" do
+    first_ten_topics = @topics[0,10]
+    
+    rendered.should have_selector('table tr') do |row|
+      first_ten_topics.each do |topic|
+        row.should have_selector('td', :content => topic.title)
+      end
+    end
+    
+  end
+  
+  it "should not display more than ten topics"
   
 end
