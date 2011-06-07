4f7d1b3c7cff69acc4ae848999a8833131a5a078
Tobias Eilert <tobias.eilert@tux-mail.de>
2011-06-07 20:53:09 +0200

    some refactoring :) added before_filter to PostsController [#5]

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,11 @@
 class PostsController < ApplicationController
 
+  before_filter :authenticate_user!
+
   def create
     
     @post = current_user.posts.build(params[:post])
+    
     if @post.save
       flash[:success] = t("Thank you for your post.")
     else
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,9 +5,9 @@ class TopicsController < ApplicationController
     @topic = Topic.find params[:id]
     @posts = @topic.posts
     
-    if user_signed_in?
-      @post = Post.new :user => current_user, :topic => @topic
-    end
+    @post = Post.new
+    
+    @post.topic = @topic if user_signed_in?
     
   end
 
