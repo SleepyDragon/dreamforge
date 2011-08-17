476c604be6ff81d2aef660be2d91d0dda26de6a0
moonglum <moonglum@moonbeamlabs.com>
2011-08-17 22:30:10 +0200

    Transfered Forum Model Spec to Minitest

--- spec/models/forum_spec.rb
+++ spec/models/forum_spec.rb
@@ -1,33 +1,31 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Forum do
-  
-  let(:valid_category) { Category.new(:name => 'wonderful name') }
-  let(:forum_without_name) { Forum.new(:subtitle => 'my subtitle', :category => valid_category) }
-  let(:forum_without_subtitle) { Forum.new(:name => 'my name', :category => valid_category) }
-  let(:forum_without_category) { Forum.new(:name => 'my name', :subtitle => 'my subtitle') }
-  
-  let(:forum_with_name_and_category) { Forum.new(:name => 'my name', :category => valid_category) }
-  let(:dependend_topic) { Topic.new(:title => "my name", :forum => forum_with_name_and_category) }
-  
-  it "does require a name" do
-    forum_without_name.should_not be_valid
-  end
-  
-  it "doesn't require a subtitle" do
-    forum_without_subtitle.should be_valid
-  end
-  
-  it "does require a category" do
-    forum_without_category.should_not be_valid
-  end
-  
-  it "does destroy all dependent topics" do
-    forum_with_name_and_category.save
-    dependend_topic.save
-    Topic.all.should include(dependend_topic)
-    forum_with_name_and_category.destroy
-    Topic.all.should_not include(dependend_topic)
-  end
-  
+    it "does validate a forum with all information" do
+      forum_with_all_information = Fabricate.build(:forum)
+      forum_with_all_information.must_be_valid
+    end
+
+    it "does require a name" do
+      forum_without_a_name = Fabricate.build(:forum, :name => nil)
+      forum_without_a_name.wont_be_valid
+    end
+
+    it "doesn't require a subtitle" do
+      forum_without_a_subtitle = Fabricate.build(:forum, :subtitle => nil)
+      forum_without_a_subtitle.must_be_valid
+    end
+
+    it "does require a category" do
+      forum_without_a_category = Fabricate.build(:forum, :category => nil)
+      forum_without_a_category.wont_be_valid
+    end
+
+    it "does destroy all dependend topics" do
+      forum = Fabricate(:forum)
+      forum.topics.each { |topic| topic.save }
+      Topic.all.wont_be_empty
+      forum.destroy
+      Topic.all.must_be_empty
+    end
 end
