b815d05f4403383e9928d7157fdfc70b51b29348
moonglum <moonglum@moonbeamlabs.com>
2011-08-17 22:52:57 +0200

    Transferred Category Model Spec to Minitest

--- spec/fabricators/category_fabricator.rb
+++ spec/fabricators/category_fabricator.rb
@@ -1,3 +1,4 @@
 Fabricator(:category) do
   name "wonderful name"
+  forums(:count => 3) { |category, i| Fabricate(:forum, :category => category) }
 end
---
+++ spec/models/category_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+describe Category do
+  it "does validate a category with all information" do
+    category_with_all_information = Fabricate.build(:category)
+    category_with_all_information.must_be_valid
+  end
+
+  it "does require a name" do
+    category_without_a_name = Fabricate.build(:category, :name => nil)
+    category_without_a_name.wont_be_valid
+  end
+
+  it "doesn't require a subtitle" do
+    category_without_a_subtitle = Fabricate.build(:category, :subtitle => nil)
+    category_without_a_subtitle.must_be_valid
+  end
+
+  it "does destroy all dependent forums" do
+    category = Fabricate.build(:category)
+    category.forums.each { |forum| forum.save }
+    Forum.all.wont_be_empty
+    category.destroy
+    Forum.all.must_be_empty
+  end
+end
