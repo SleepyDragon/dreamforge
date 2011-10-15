f202cc2dda46f83f17f675360d7d50e83b790fd3
Tobias Eilert <tobias.eilert@tux-mail.de>
2011-10-15 14:53:04 +0200

    Migrated the rule spec to minitest :)

---
+++ spec/fabricators/rule_fabricator.rb
@@ -0,0 +1,4 @@
+Fabricator(:rule) do
+  name "the rule of the seven seas"
+  description "thou shalt sail over shaky waves"
+end
---
+++ spec/models/rule_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Rule do
+  
+  it "should be valid" do
+    rule = Fabricate.build(:rule)
+    rule.must_be_valid
+  end
+  
+  it "should have a name" do
+    rule_without_a_name = Fabricate.build(:rule, name: nil)
+    rule_without_a_name.wont_be_valid
+  end
+  
+  it "should not have actions" do
+    rule_with_actions = Fabricate.build(:rule, actions: "pirateers!")
+    rule_with_actions.wont_be_valid
+  end
+  
+  it "should not have a humble, fictional author" do
+    rule_with_author = Fabricate.build(:rule, fictional_author: "Blackbeard")
+    rule_with_author.wont_be_valid
+  end
+  
+  it "should be of type 'Rule'" do
+    rule = Fabricate.build(:rule)
+    rule.type.must_equal "Rule"
+    
+    # No type other than 'Rule' is allowed.
+    ["rule", "pirate", "another type"].each do |type|
+      rule.type = type
+      rule.wont_be_valid
+    end
+  end
+  
+end
