105eac0e671a996c430dd5a172a4b1bd43c9d64b
moonglum <moonglum@moonbeamlabs.com>
2011-10-16 16:17:37 +0200

    Little Fabricator Update
    Categories and Forums create corresponding 'children' now

--- test/fabricators/category_fabricator.rb
+++ test/fabricators/category_fabricator.rb
@@ -1,4 +1,4 @@
 Fabricator(:category) do
   name "wonderful name"
-  forums(:count => 3) { |category, i| Fabricate(:forum, :category => category) }
+  forums!(:count => 3) { |category, i| Fabricate(:forum, :category => category, :name => "#{category.name} #{i}") }
 end
