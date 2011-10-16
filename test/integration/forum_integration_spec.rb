eb8b610bb4549bfb5088c42d9e3ceba856bb643d
moonglum <moonglum@moonbeamlabs.com>
2011-10-16 17:14:53 +0200

    Finished converting Forum#show to MiniTest Integration
    There is one failing and one skipping test, because pagination is
    currently not implemented.

--- test/integration/forum_integration_spec.rb
+++ test/integration/forum_integration_spec.rb
@@ -37,6 +37,53 @@ describe "Forum integration" do
         page.must_have_link_to forum_path(@categories.first.forums.first)
       end
     end
+  end
+
+  describe "show" do
+
+    before do
+      @forum = Fabricate :forum, :topics => []
+
+      20.times do |topic_id|
+        @forum.topics << Fabricate(:topic, :title => "Topic #{topic_id}", :forum => @forum)
+      end
+
+      visit forum_path(@forum)
+    end
+
+    it "should be successful" do
+      page.status_code.must_equal 200
+    end
+
+    it "should display the correct headline" do
+      within "h2" do
+        page.must_have_content @forum.name
+      end
+    end
+
+    it "should display the correct breadcrumbs" do
+      within ".breadcrumbs" do
+        page.must_have_content @forum.name
+        page.must_have_content @forum.category.name
+        page.must_have_link_to forums_path
+      end
+    end
+
+    it "should display a table with the first ten topics" do
+      within ".topics tbody" do
+        10.times do |id|
+          page.must_have_content "Topic #{id}"
+        end
+      end
+    end
+
+    it "should only show the first ten topic" do
+      within ".topics tbody" do
+        page.wont_have_content "Topic 10"
+      end
+    end
+
+    it "should show pagination for more topics"
 
   end
 
