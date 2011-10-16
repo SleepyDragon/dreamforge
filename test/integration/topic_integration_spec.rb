e02957090d4c32729f99d78e853a685b8c2a4fc4
Tobias Eilert <tobias.eilert@tux-mail.de>
2011-10-16 15:19:02 +0200

    Added more tests to the topic integration spec. Also aded a helper method to Capybara::DSL, which enables you to sign in using devise (since the devise TestHelpers-module is not accessible from within integration tests).

--- test/integration/topic_integration_spec.rb
+++ test/integration/topic_integration_spec.rb
@@ -1,17 +1,54 @@
 
-describe 'Topic integration' do
+
+describe 'Topic integration' do  
+    
   
   describe "show" do
     before do
-      topic = Fabricate(:topic)
-      visit topic_path(topic)
+      @topic = Fabricate(:topic)
+      visit topic_path(@topic)
     end
     
     it "should be successful" do
       page.status_code.must_equal 200
     end
     
-    it "should show the topic title in big, shiny and readable letters"
+    it "should show the topic title in big, shiny and readable letters" do
+      within 'h2' do
+        page.must_have_content @topic.title
+      end
+    end
+    
+    describe "not logged in" do
+      it "will not greet the user" do
+        within '#userbox' do
+          page.wont_have_content "Welcome "
+        end
+      end
+      
+      it "will give you the possibility to log in" do
+        within '#userbox' do
+          page.must_have_selector 'a'
+          page.must_have_content 'login'
+        end
+      end
+    end
+    
+    describe "logged in" do
+      before do
+        @user = Fabricate(:user)
+      end
+      
+      it "will greet the user" do
+        sign_in @user
+        visit topic_path(@topic)
+        
+        within '#userbox' do
+          page.must_have_content "Welcome #{@user.name}"
+        end
+      end
+    end
+    
   end
 
 end
--- test/support/custom_capybara_expectations.rb
+++ test/support/custom_capybara_expectations.rb
@@ -1,9 +1,28 @@
 class Capybara::Session
+
   def has_flash_message?(message)
     within '#flash' do
       has_content? message
     end
   end
+  
 end
 
 CapybaraMiniTestSpec::Matcher.new(:has_flash_message?)
+
+
+
+module Capybara::DSL
+  
+  # Signs a user in by filling the form and submitting.
+  # This enables you to get a similar effect as the devise helper
+  # modules, which are not accessible from within integration tests.
+  def sign_in(user)
+    visit user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    
+    click_button 'Sign in'
+  end
+  
+end
